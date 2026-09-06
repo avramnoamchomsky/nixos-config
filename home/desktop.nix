094b8dfa8329740db92a3052d8ef1a9e19b132d0
chomsky <315812871+avramnoamchomsky@users.noreply.github.com>
2026-09-06 16:14:03 +0800

    tune super battery fan curve

--- home/desktop.nix
+++ home/desktop.nix
@@ -75,9 +75,9 @@
     text = ''
       [Settings]
       UserMode=super_battery_mode
-      fan1SpeedSettings=30|40|40|50|57|65|70
-      fan1TempSettings=50|57|64|71|78|85
-      fan2SpeedSettings=20|48|55|62|68|77|85
+      fan1SpeedSettings=30|30|30|30|30|30|30
+      fan1TempSettings=50|55|60|65|70|75
+      fan2SpeedSettings=30|30|30|30|30|30|30
       fan2TempSettings=55|60|65|70|75|80
       fanModeAdvanced=true
     '';
